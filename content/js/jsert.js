f95bba06aba020fdd35b4b30ef2bae4178c10313
Joseph Morukhuladi <morukhuladi.j.kabelo@gmail.com>
2025-01-03 12:05:39 +0200

    Updating folder structure

--- content/js/jsert.js
+++ content/js/jsert.js
@@ -8,7 +8,13 @@
  * Class for creating Jsert objects
  */
 class Jsert {
-    constructor() {
+    /**
+     * Creates an instance of the Jsert class
+     * @param {string} group 
+     * @returns 
+     */
+    constructor(group) {
+        this.group = group;
         this.tests = [];
         this.passed = [];
         this.failed = [];
@@ -34,14 +40,17 @@ class Jsert {
      * Runs all tests added to the tests list
      */
     run() {
-        console.log("Jsert Running Tests...");
-        let label = "%cCompleted In";
+        let label = "Completed In";
+        let b = this._blue();
         console.time(label);
+        console.log("%cJsert instance info", b);
+        console.log(`%cGroup Name: ${this.group}`, b);
+        console.log("Jsert Running Tests");
         this.tests.forEach(function(t) { t.test() });
-        console.log("Jsert Completed Running Tests");
-        console.log("Loading summary...");
+        console.log("%cJsert Completed Running Tests", b);
         this.summary();
-        console.timeLog(label, this._green());
+        console.timeEnd(label, this._green());
+        this.reset();
     }
     /**
      * Adds test to the tests list
@@ -59,9 +68,17 @@ class Jsert {
      */
     summary() {
         let style = this._green();
-        console.log(`%cTotal Tests Executed: ${ this.passed.length + this.failed.length }`, style);
-        console.log(`%cTotal Tests Passed: ${ this.passed.length}`, style);
-        console.log(`%cTotal Tests Failed: ${ this.failed.length}`, style);
+        console.log("Loading summary");
+        console.log(`%cTotal Tests Executed :${ this.passed.length + this.failed.length }`, style);
+        console.log(`%cTotal Tests Passed   :${ this.passed.length}`, style);
+        console.log(`%cTotal Tests Failed   :${ this.failed.length}`, style);
+    }
+    /**
+     * Resets all test results
+     */
+    reset() {
+        this.passed = [];
+        this.failed = [];   
     }
     _green() {
         return "color:#5f5;"
---
+++ content/js/screenshot.js
@@ -0,0 +1,20 @@
+    
+    //adding & fetching a JSON object with Qwery.js
+    const qwery = new Qwery({ name: "app_data" }).create();
+    const appInfo = {
+        id: 1,
+        OS: "Windows",
+        key: "V1-3442-AFD3-5221"
+    }
+    //adding entry to the 'app-info' dataset
+    qwery.add({ dataset: "app-info", data: appInfo});
+    //fetching entry by id, from the 'app-info' dataset
+    const item = qwery.get({ dataset: "app-info", field: "id", value: 1 });
+    //item will be a JSON object if it is found
+    /*
+    {
+        "id": 1,
+        "OS": "Windows",
+        "key": "V1-3442-AFD3-5221"
+    }
+    */
